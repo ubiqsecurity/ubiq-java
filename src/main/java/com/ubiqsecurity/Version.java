8334156f88d58e35224bd8e3ceebfcae66a605bb
Gary Schneir <gary@ubiqsecurity.com>
2025-06-02 17:39:56 -0700

    Missed commit of generated file

--- src/main/java/com/ubiqsecurity/Version.java
+++ src/main/java/com/ubiqsecurity/Version.java
@@ -2,5 +2,5 @@
 // Built during the gradle build process using the VERSION information in gradle.properties
 package com.ubiqsecurity;
 public final class Version { 
-   public static String VERSION = "2.2.5";
+   public static String VERSION = "2.2.6";
 }
